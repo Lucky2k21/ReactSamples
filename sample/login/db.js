a1987c3e172fd80987afa45fd09f0b177323c32b
Lucky2k21 <lakshmi.pg@outlook.com>
2023-02-18 13:37:02 -0800

    login

--- sample/login/db.js
+++ sample/login/db.js
@@ -1,2 +1,3 @@
 let mongoose =require('mongoose');
-mongoose.connect('mongodb://localhost:27017/pro')
+mongoose.connect('mongodb+srv://test:test123@cluster0.hpg6u3o.mongodb.net/?retryWrites=true&w=majority/test', { useNewUrlParser: true })
+
